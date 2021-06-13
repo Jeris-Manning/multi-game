22f59869cc15765ddc552d285b6ed1f819b06a32
Jeris Manning <JerisManning-at-361600009311>
2021-06-13 15:57:48 -0600

    mobile height fix

--- src/POKER/Poker.js
+++ src/POKER/Poker.js
@@ -37,6 +37,9 @@ const AppWrap = styled.div`
   background-color: #d8f0c1;
   background-image: url(${invertFlies});
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   /* border: solid pink 5px; */
 `;
 const TestDove = styled(Div100vh)`
@@ -48,14 +51,14 @@ const PokerContainer = styled.div`
   background-color: #d8f0c1;
   background-image: url(${wallFlies});
   /* min-height: 100vh; */
-  height: inherit;
+  /* height: inherit; */
+  height: 95%;
   display: flex;
   justify-content: space-between;
   flex-direction: column;
   align-items: center;
   max-width: 1200px;
-  border-left: black 5px solid;
-  border-right: black 5px solid;
+  border: black 3px solid;
   /* border: solid yellow 7px; */
   margin: 0 auto;
   @media (max-width: 1210px) {
